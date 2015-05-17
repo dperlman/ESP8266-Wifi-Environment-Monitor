220746d3756b678c0582ad6dcb365b41aea5a94f
David M. Perlman <omgoleus@gmail.com>
2015-05-16 22:44:50 -0500

    Change init.lua timers to 6 and 7 so the small number ones can be loop numbered in the ping loop

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 wifi.setmode(wifi.STATIONAP)
 print(wifi.sta.getip())
-print("Stop timer alarm 0 and 1 to interrupt")
+print("Stop timer alarm 6 and 7 to interrupt")
 
 
 
-tmr.alarm(0, 15000, 0,
+tmr.alarm(6, 15000, 0,
   function()
     dofile("envmon_reboot.lc")
   end)
 
-tmr.alarm(1, 60000, 1, 
+tmr.alarm(7, 60000, 1, 
   function() 
     dofile("envmon.lc") 
   end)
