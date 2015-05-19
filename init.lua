025ec36ce69637f9413b72586da9008bf86f6641
David M. Perlman <omgoleus@gmail.com>
2015-05-19 13:11:26 -0500

    organize init.lua and the settings variables that go there

--- init.lua
+++ init.lua
@@ -1,27 +1,31 @@
-wifi.setmode(wifi.STATIONAP)
-print(wifi.sta.getip())
-print("Stop timer alarm 0 and 1 to interrupt")
-
---keep track of count of failed connections
-pfails=0
-tfails=0
-tsuccess=true
-
---the api to use
+--the thingspeak api to use
 --dapi="YH4YN6VWTKLZN58H" --001
 --sapi="39FCF7QAGRAV1LV9" --001
 
 dapi="M8UM5QKG9S25PUCG" --002
 sapi="V4FIQ8QKOJFGCEJ8" --002
 
+--how often to run
+local howoften=60000
+
+--global variables used across files
+pfails=0
+tfails=0
+tsuccess=true
+
+--setup wifi
+wifi.setmode(wifi.STATIONAP)
+print(wifi.sta.getip())
+print("Stop timer alarm 0 and 1 to interrupt")
 
 
+--start the timers
 tmr.alarm(1, 15000, 0,
   function()
     dofile("envmon_reboot.lc")
   end)
 
-tmr.alarm(0, 60000, 1, 
+tmr.alarm(0, howoften, 1, 
   function() 
     dofile("envmon.lc") 
   end)
