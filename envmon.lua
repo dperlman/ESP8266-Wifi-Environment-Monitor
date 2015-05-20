d7b6836670265f2b3cd152e774729983f732347a
David M. Perlman <omgoleus@gmail.com>
2015-05-20 08:34:40 -0500

    Revert "take out temphum function"
    This reverts commit 8d00bea1a17539e755110385ccb7a2a3ae7c07b3.

--- envmon.lua
+++ envmon.lua
@@ -22,11 +22,64 @@ clist = {}
 
 temp2 = 0
 temperature = 0
-tempdiff = 0
+tempdiff = nil
 humidity = 0
 avms = 0
 datetime = ""
 
+
+local function temphum()
+  --print("temphum", node.heap())
+  
+  --dht22 version
+  print("pre-dht heap", node.heap())
+  therm = require("dht_lib")
+  therm.read22(3) --gpio0
+  local dhtT = therm.getTemperature() * 1000 --so it's on same scale as the ds18b20
+  humidity = therm.getHumidity()
+  local hh = tostring(humidity)
+  humidity = hh:sub(1,-2).."."..hh:sub(-1)
+  DHT = nil
+  package.loaded["dht_lib"]=nil
+  _G["dht_lib"]=nil
+  print("post-dht heap", node.heap())
+  collectgarbage()
+  print("post-dht-gc heap", node.heap())
+  
+  
+  --ds18b20 version
+  print("pre-ds heap", node.heap())
+  therm = require("ds18b20INT")
+  therm.setup(3) --gpio0
+  therm.readNumber()
+  therm.readNumber()
+  local dsT=therm.readNumber()
+  if dsT == 850000 then
+    dsT = nil
+  end
+  therm=nil
+  package.loaded["ds18b20INT"]=nil
+  _G["ds18b20INT"]=nil
+  print("post-ds heap", node.heap())
+  collectgarbage()
+  print("post-ds-gc heap", node.heap())
+  
+  local tt
+  --calc diff
+  if dhtT and dsT then
+    tempdiff = dsT - dhtT
+    tt = '0'..tostring(tempdiff)
+    tempdiff = tt:sub(1,-5).."."..tt:sub(-4)
+  end
+
+  --clean up the numbers 
+  tt = tostring(dhtT)
+  temperature = tt:sub(1,-5).."."..tt:sub(-4)
+  tt = tostring(dsT)
+  temp2 = tt:sub(1,-5).."."..tt:sub(-4)
+  
+  --print("package.loaded done", node.heap())
+end
 
 
 
@@ -66,6 +119,7 @@ end
 
 
 
+temphum()
 
 for i,host in pairs(hostlist) do
   startping(i, host)
